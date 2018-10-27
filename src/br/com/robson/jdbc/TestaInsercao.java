81503cfbbcbac1a5d1a85ee0486e5f47d2c3c4a9
Robson Marinho <robson_luz15@hotmail.com>
2018-10-27 01:08:58 -0300

    Transacoes:commit e rollback

--- src/br/com/robson/jdbc/TestaInsercao.java
+++ src/br/com/robson/jdbc/TestaInsercao.java
@@ -9,17 +9,41 @@ import java.sql.Statement;
 public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
+
 		String nome = "Notebook i5";
 		String descricao = "Notebook i5";
 
-		Connection connection = Database.getConnection();
+		// Ativando conexao
+		try (Connection connection = Database.getConnection()) {
+			// Se quiser desativar
+			connection.setAutoCommit(false);
+			String sql = "insert into Produto (nome, descricao) values (?, ?)";
 
-		String sql = "insert into Produto (nome, descricao) values (?, ?)";
-		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS );
-		statement.setString(1,  nome);
-		statement.setString(2,  descricao);
-		
-		boolean resultado = statement.execute(sql, Statement.RETURN_GENERATED_KEYS);
+			try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+				// Insert into no DB
+				adiciona("TV LCD", "32 polegadas", statement);
+				adiciona("Blueray", "Full HGMI", statement);
+
+				connection.commit();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				connection.rollback();
+				System.out.println("Rollback efetuado");
+			}
+		}
+	}
+
+	private static void adiciona(String nome, String descricao, PreparedStatement statement) throws SQLException {
+
+		if (nome.equals("Blueray")) {
+			throw new IllegalArgumentException("Problema ocorrido");
+		}
+
+		statement.setString(1, nome);
+		statement.setString(2, descricao);
+
+		boolean resultado = statement.execute();
 		System.out.println(resultado);
 
 		ResultSet resultSet = statement.getGeneratedKeys();
@@ -28,7 +52,6 @@ public class TestaInsercao {
 			System.out.println(id + " gerado");
 		}
 
-		statement.close();
-		connection.close();
+		resultSet.close();
 	}
 }
